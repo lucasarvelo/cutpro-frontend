d1e8b3c9179495a398463ffc4db626cc89a2330d
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-14 15:55:06 -0400

    Remove InitialState from createStore Redux

--- src/store.js
+++ src/store.js
@@ -3,10 +3,6 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
-export default function configureStore(initialState = {}) {
-  return createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(thunk))
-  );
+export default function configureStore() {
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 }
