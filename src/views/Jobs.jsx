2d91060a8c6b8aabb03f3ba5aaa2741aa987b4a5
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-21 00:26:45 -0400

    Update Jobs view with new components

--- src/views/Jobs.jsx
+++ src/views/Jobs.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import JobsForm from "../components/JobsForm/JobsForm";
+import JobsTable from "../components/JobsTable/JobsTable";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-4 p-4">
+          <JobsForm />
+        </div>
+        <div className="col-md-8 p-4">
+          <JobsTable />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
