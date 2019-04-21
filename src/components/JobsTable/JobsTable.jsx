ae2e6e74b620a391f6ea2113245fd4f6eb324253
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-21 00:25:56 -0400

    Added JobTable Component

---
+++ src/components/JobsTable/JobsTable.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = state => {
+  return state.jobsReducer;
+};
+class JobsTable extends Component {
+  render() {
+    return (
+      <table className="table table-sm table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Jobs Number</th>
+            <th scope="col">Client Number</th>
+            <th scope="col">Client Name</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.jobs.map((job, index) => {
+            return (
+              <tr key={"job" + index}>
+                <th scope="row" className="align-middle">
+                  {index + 1}
+                </th>
+                <td className="align-middle">{job.jobNumber}</td>
+                <td className="align-middle">{job.clientNumber}</td>
+                <td className="align-middle">{job.clientName}</td>
+                <td className="align-middle">
+                  <button className="btn btn-outline-primary btn-sm m-1">
+                    Edit
+                  </button>
+                  <button className="btn btn-outline-danger btn-sm m-1">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(JobsTable);
