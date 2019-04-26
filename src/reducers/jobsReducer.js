a3baa17e19f61384530b2388e3c052dd1401807a
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-25 20:33:03 -0400

    Added Sample Data to initial  state & new actions

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -1,4 +1,51 @@
-const initialState = { jobs: [], loading: false, error: null };
+const initialState = {
+  jobs: [
+    {
+      jobNumber: 123456,
+      clientNumber: 123456,
+      clientName: "ddsd",
+      formChecked: true,
+      windows: [
+        {
+          width: 2.256,
+          height: 12.256,
+          quantity: "4"
+        },
+        {
+          width: 2.256,
+          height: 12.256,
+          quantity: "4"
+        },
+        {
+          width: 2.256,
+          height: 12.256,
+          quantity: "4"
+        },
+        {
+          width: 2.256,
+          height: 12.256,
+          quantity: "4"
+        }
+      ]
+    },
+    {
+      jobNumber: 1234,
+      clientNumber: 1234,
+      clientName: "hello",
+      formChecked: true,
+      windows: []
+    },
+    {
+      jobNumber: 4567895,
+      clientNumber: 4568754,
+      clientName: "Paco",
+      formChecked: true,
+      windows: []
+    }
+  ],
+  loading: false,
+  error: null
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
@@ -6,15 +53,61 @@ export default (state = initialState, action) => {
       return {
         ...state,
         jobs: state.jobs
-          .filter(job => job.id !== action.payload.id)
+          .filter(job => job.jobNumber !== action.payload.id)
           .push(action.payload)
       };
     case "ADD_JOB":
       return { ...state, jobs: [...state.jobs, action.payload] };
-    case "REMOVE_JOB":
+    case "DELETE_JOB":
       return {
         ...state,
-        jobs: state.jobs.filter(job => job.id !== action.payload)
+        jobs: state.jobs.filter(
+          job => job.jobNumber !== action.payload.jobNumber
+        )
+      };
+    case "ADD_WINDOW":
+      return {
+        ...state,
+        jobs: state.jobs.map(job => {
+          if (job.jobNumber === action.payload.jobNumber) {
+            return { ...job, windows: [...job.windows, action.payload.window] };
+          }
+          return job;
+        })
+      };
+    case "UPDATE_WINDOW":
+      return {
+        ...state,
+        jobs: state.jobs.map(job => {
+          if (job.jobNumber === action.payload.jobNumber) {
+            return {
+              ...job,
+              windows: job.windows.map((window, index) => {
+                if (index === action.payload.windowIndex) {
+                  return action.payload.window;
+                }
+                return window;
+              })
+            };
+          }
+          return job;
+        })
+      };
+
+    case "DELETE_WINDOW":
+      return {
+        ...state,
+        jobs: state.jobs.map(job => {
+          if (job.jobNumber === action.payload.jobNumber) {
+            return {
+              ...job,
+              windows: job.windows.filter(
+                (window, index) => index !== action.payload.windowIndex
+              )
+            };
+          }
+          return job;
+        })
       };
     case "FETCH_JOBS_BEGIN":
       return { ...state, loading: true };
