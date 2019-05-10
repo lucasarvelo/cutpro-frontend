8334130b6cf61cde0b0accc1e8d949cb60a289e1
Lucas Arvelo <lucasarvelo@gmail.com>
2019-05-09 22:27:24 -0400

    Added About Text

---
+++ src/components/About/AboutInfo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AboutInfo() {
+  return (
+    <div>
+      <p>
+        Proof of concept for an app to admin product orders for a windows
+        production company and generate optimized cutting list for windows
+        parts.
+      </p>
+      <p>Build by Lucas Arvelo</p>
+    </div>
+  );
+}
+
+export default AboutInfo;
