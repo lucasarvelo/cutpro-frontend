7d308f71216c8caae7f4fcdd1714af18aa4be54b
Lucas Arvelo <lucasarvelo@gmail.com>
2019-06-01 20:07:23 -0400

    Added Cutting Lists to the Redux store

---
+++ src/actions/cutListActions.js
@@ -0,0 +1,30 @@
+import { getId } from "../services/idGenerator/idGenerator";
+
+export const addCuttingList = data => dispatch => {
+  const id = getId();
+  const windowsTotal = data.orders
+    .map(order => order.windows.length)
+    .reduce((windowsTotal, orderTotal) => windowsTotal + orderTotal);
+
+  dispatch({
+    type: "ADD_CUTTING_LIST",
+    payload: {
+      poNumber: id,
+      ordersTotal: data.orders.length,
+      windowsTotal: windowsTotal,
+      dateCreation: new Date().toLocaleDateString("en-US"),
+      options: {
+        marginError: data.cuttingList[0].marginError,
+        bladeThickness: data.cuttingList[0].bladeThickness
+      },
+      cuttingList: data.cuttingList
+    }
+  });
+};
+
+export const removeCuttingList = poNumber => dispatch => {
+  dispatch({
+    type: "REMOVE_CUTTING_LIST",
+    payload: { poNumber: poNumber }
+  });
+};
